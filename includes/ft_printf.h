e85215556cfc9a9a4a529d1e3ce246fc0d5225ea
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-08-06 14:24:19 +0200

    for testing purposes

---
+++ includes/ft_printf.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpirzent <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/04 15:41:26 by jpirzent          #+#    #+#             */
+/*   Updated: 2018/08/06 10:02:13 by jpirzent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# define UINT_MX 4294967296
+
+int		ft_printf(char *format, ...);
+int		pr_handle(char fmt, va_list list);
+int		pr_ints(char fmt, va_list list);
+int		pr_chars(char fmt, va_list list);
+int		pr_base(char fmt, va_list list);
+int		pr_wchars(char fmt, va_list list);
+int		pr_cints(char fmt, va_list list);
+int		pr_padd(va_list list);
+
+int		pr_printaddress(void *n);
+int		pr_putstr(char *s);
+int		pr_putwstr(wchar_t *ws);
+int		pr_putchar(char c);
+int		pr_putnbr(long n);
+int		pr_putuint(long n);
+int		pr_putcx(long x);
+int		pr_putnbr_b(long n, int b);
+char	*pr_itoa_b(long long n, int b);
+char	*pr_itoa(long n);
+
+#endif
---
+++ sources/test.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpirzent <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/04 16:27:32 by jpirzent          #+#    #+#             */
+/*   Updated: 2018/08/06 14:18:31 by jpirzent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+int		main(void)
+{
+//	int		i = 0;
+//	char	s[] = "kay ime cool";
+//	char	c = 'c';
+//	int		*p;
+//	int		l = -123;
+
+//	p = &i;
+
+	printf("%d\n", ft_printf("printf: %i\n", NULL));
+//	printf("%d\n", printf("printf: %i\n", NULL));
+}
